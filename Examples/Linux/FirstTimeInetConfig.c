ebc36e356a0adbeb7d2d3d3e5a604d14709b00cb
Tuan Anh Pham <txp112330@utdallas.edu>
2016-01-26 06:11:25 +0000

    Revert "Merge pull request #2 from j1nb7lek/dev"
    This reverts commit 7f14a08fea3090252238a871540f0f69f6fe9f29, reversing
    changes made to f8e250c07b5d9a53492473314e597682329ba85e.

--- Examples/Linux/FirstTimeInetConfig.c
+++ Examples/Linux/FirstTimeInetConfig.c
@@ -5,16 +5,16 @@ int main(){
 	if(mmInit(mm)){
 		if(mmInitGPRS(mm)){
 			printf("Successfully initialized.\n");
-			free(mm);
+			delete mm;
 			return 0;
 		}else{
 			printf("Initialization failed.\n");
-			free(mm);
+			delete mm;
 			return -1;
 		}
 	}else{
 		printf("Unable to initialize connection to modem.\n");
-		free(mm);
+		delete mm;
 		return -1;
 	}
 }
