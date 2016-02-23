c90f9f5fb619e45699065ef3743fdb4389f3ae50
j1nb7lek <jabinkle@gmail.com>
2016-02-22 22:59:52 -0600

    Update cModemInterface.h
    To Compile on Launchpad

--- MakerModemLib/cModemInterface.h
+++ MakerModemLib/cModemInterface.h
@@ -1,3 +1,20 @@
+/*
+    This file is part of the MakerModem API.
+
+    The MakerModem API is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The MakerModem API is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the MakerModem API.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* Modem Interface class
  * Handles high-level functions for communicating with modem
  * #define [MICROCONTROLLER] before including ModemInterface.h
@@ -19,6 +36,11 @@ struct ModemInterface {
 	int dlmode;
 	int disconnectIndex;
 };
+#ifdef LAUNCHPAD
+/*
+* TODO: Launchpad
+*/
+#endif
 
 #ifdef ARDUINO
 bool mmSendCommandCheckForOkay(struct ModemInterface* interf, FlashStringHelper cmd);
@@ -35,9 +57,10 @@ struct ModemInterface * mmCreate(char* blkdvc);
 #endif
 
 
-int mmInit(struct ModemInterface* interf);
-int mmCheckForConnection(struct ModemInterface* interf);
-int mmSendCommandCheckForOkay(struct ModemInterface* interf, char* cmd);
+bool mmInit(struct ModemInterface* interf);
+bool mmCheckForConnection(struct ModemInterface* interf);
+bool mmCheckForOkay(struct ModemInterface* interface);
+bool mmSendCommandCheckForOkay(struct ModemInterface* interf, char* cmd);
 void mmSendCommand(struct ModemInterface* interf, char* cmd);
 //void mmSendCommand(struct ModemInterface* interf, int cmd);
 int mmGetResponse(struct ModemInterface* interf, char* buffer, int length);
