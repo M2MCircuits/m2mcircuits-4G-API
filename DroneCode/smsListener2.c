39808eefa72ddd0f26a19a4878ab84e15ecfc5d2
j1nb7lek <jabinkle@gmail.com>
2016-04-18 22:58:18 -0500

    Update smsListener2.c

--- DroneCode/smsListener2.c
+++ DroneCode/smsListener2.c
@@ -20,6 +20,22 @@
 
 #include "cSMS.h"
 
+/*
+//Flight Plan at SPN
+	float route_one[][6] = {
+		{MAV_CMD_MISSION_START,  	0, 		0,  	0,  	0,	0, 		  	0},//start in AUTO mode without need to touch the throttle on the controller
+		//	CMD					P1		P2		P3		Alt		Lat			Long
+		{MAV_CMD_NAV_TAKEOFF,  	0, 		0,  	0,  	3.0,	0, 		  	0},					//  go up three feet
+		
+		{MAV_CMD_NAV_WAYPOINT, 	0, 		0,		0,  	3.0, 	32.994596, -96.751628},			//  south-west corner
+		{MAV_CMD_NAV_WAYPOINT, 	0, 		0,		0,  	3.0, 	32.994596, -96.751528},			//  south-east corner
+		{MAV_CMD_NAV_WAYPOINT, 	0, 		0,		0,  	3.0, 	32.994696, -96.751528},			//  north-east corner
+		{MAV_CMD_NAV_WAYPOINT, 	0, 		0,		0,  	3.0, 	32.994696, -96.751628},			//  north-west corner
+	
+		{MAV_CMD_NAV_LAND, 	 	0,		0,		0,  	0,		0,          0}				//  go back down three feet
+	};
+*/
+
 /* Print the last 5 received messages for testing */
 void messagesHead(int count, int* messageIds, struct ModemInterface *mm) {
   char phoneNumber[11];
